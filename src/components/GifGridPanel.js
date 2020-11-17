078cbe2d3c891a4d0ed640b04112256edd8e924a
GalvisDeveloper <galvisdeveloper@gmail.com>
2020-11-16 21:08:36 -0500

    tests to GifGridPanel component

--- src/components/GifGridPanel.js
+++ src/components/GifGridPanel.js
@@ -1,13 +1,19 @@
 
 
 import React from 'react'
+import PropTypes from 'prop-types';
 
-export const GifGridPanel = ({url, title}) => {
+export const GifGridPanel = ({ url, title }) => {
 
     return (
-        <div className = "card animate__animated callout-subtitle animate__animated animate__zoomInDown">
+        <div className="card animate__animated callout-subtitle animate__animated animate__zoomInDown">
             <p>Gif: {title}</p>
             <img src={url} alt={title} />
         </div>
     )
+}
+
+GifGridPanel.propTypes = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
 }
---
+++ src/tests/components/GifGridPanel.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+const { shallow } = require("enzyme")
+import { GifGridPanel } from '../../components/GifGridPanel';
+import '@testing-library/jest-dom';
+
+
+describe('Pruebas en el <GifGridPanel />', () => {
+
+    const title = "Titulo prueba";
+    const url = "https://localhost/algo.jpg";
+    const wrapper = shallow(<GifGridPanel url={url} title={title} />);
+
+    test('Debe mostrar el componente correctamente', () => {
+        expect(wrapper).toMatchSnapshot();
+    })
+
+    test('Debe contener un parrafo con el titulo y el (Gif:) antes de el', () => {
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe("Gif: " + title);
+    })
+
+    test('Testeando la imagen', () => {
+        const img = wrapper.find('img');
+        expect(img.props().src).toBe(url);
+        expect(img.props().alt).toBe(title);
+    })
+
+    test('Debe tener (animate__zoomInDown)', () => {
+        const div = wrapper.find('div');
+        const classDiv = div.prop('className');
+
+        expect(classDiv.includes('animate__zoomInDow')).not.toBe(false);
+    })
+
+})
