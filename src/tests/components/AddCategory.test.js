09a00c0dcbfc29b92eaed69ef67cfcc0875c49d5
GalvisDeveloper <galvisdeveloper@gmail.com>
2020-11-17 19:32:34 -0500

    Responsive done

---
+++ src/tests/components/AddCategory.test.js
@@ -0,0 +1,11 @@
+
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import "@testing-library/jest-dom";
+
+describe('Testeando el componente AddCategory', () => {
+    test('should ', () => {
+
+    })
+});
