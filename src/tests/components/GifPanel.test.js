16d8c6fea86140b3228afeb8b70500143b530607
GalvisDeveloper <galvisdeveloper@gmail.com>
2021-01-10 12:42:06 -0500

    Addin GifPanel test file

---
+++ src/tests/components/GifPanel.test.js
@@ -0,0 +1,42 @@
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import GifPanel from '../../components/GifPanel';
+import '@testing-library/jest-dom';
+import { useFetchGifs } from '../../hooks/useFetchGifs';
+jest.mock('../../hooks/useFetchGifs');
+
+
+describe('Pruebas en el componente <GifPanel />', () => {
+
+    const category = 'Moto gp';
+
+    test('Prueba 1: Coincidir el snapshot ', () => {
+
+        useFetchGifs.mockReturnValue({
+            data: [],
+            loading: true
+        });
+        const wrapper = shallow(<GifPanel category={category} />);
+        expect(wrapper).toMatchSnapshot();
+    })
+
+    test('Prueba 2: Mostrar items cuando se cargan las imagenes useFetchGifs(customHook)', () => {
+
+        const gifs = [{
+            id: '01',
+            url: 'https://www.google.com/',
+            title: 'Google'
+        }]
+
+        useFetchGifs.mockReturnValue({
+            data: gifs,
+            loading: false
+        });
+
+        const wrapper = shallow(<GifPanel category={category} />);
+        expect(wrapper).toMatchSnapshot();
+    })
+
+
+});
